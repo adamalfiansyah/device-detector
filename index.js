151fe1596064200893ee97770f4901131697401f
Adam Alfiansyah <adamalfiansyah@gmail.com>
2021-11-23 12:22:26 +0700

    Create env

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import Mongoose from 'mongoose';
 import ArticleController from "./controllers/ArticleController.js";
+import dotenv from "dotenv";
 
-const APP_PORT  = 8080
-const app       = express()
+const env = dotenv.config();
+const app = express()
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -12,7 +13,7 @@ app.use(express.urlencoded({extended: true}));
 app.post('/api/articles', ArticleController.store);
 
 //koneksi ke MongoDB server
-Mongoose.connect('mongodb+srv://wegodev:e94ry437r3he32re@wegodev.7x0xp.mongodb.net/belajar?retryWrites=true&w=majority', {        
+Mongoose.connect(process.env.DB_URL, {        
   useNewUrlParser     : true, 
   useUnifiedTopology  : true,
   autoIndex: true
@@ -22,6 +23,6 @@ db.on('error', (err) => { console.error(err) });
 db.once('open', () => { console.log('MongoDB connected') });
 
 //listen port
-app.listen(APP_PORT, function () {
-  console.log(`Server listening on port ${APP_PORT}`);
+app.listen(process.env.APP_PORT, function () {
+  console.log(`Server listening on port ${process.env.APP_PORT}`);
 });
